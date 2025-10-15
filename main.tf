b569036709bbd7174573332ee8e1e253fe6c627f
arundhakad016 <arundhakad016@gmail.com>
2025-10-15 06:54:55 +0000

    Updated files for creating rg, storage account and container

--- main.tf
+++ main.tf
@@ -1,15 +1,25 @@
-# Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
-    }
-  }
-
-  required_version = ">= 1.1.0"
+# 1. Create Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-terraform-storage"
+  location = "Central India"
 }
 
-provider "azurerm" {
-  features {}
+# 2. Create Storage Account
+resource "azurerm_storage_account" "storage" {
+  name                     = "arundemostorageacct"   # must be globally unique
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+# 3. Create Blob Container
+resource "azurerm_storage_container" "container" {
+  name                  = "terraformstate"
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
 }
---
+++ terraform.tf
@@ -0,0 +1,9 @@
+## Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.2"
+    }
+  }
+}
