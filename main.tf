f9b7144ee3bf67ab52ae10817bc7f53c7e479518
arundhakad016 <arundhakad016@gmail.com>
2025-11-08 17:27:14 +0000

    Working code for Azure Linux VM

--- main.tf
+++ main.tf
@@ -41,6 +41,38 @@ resource "azurerm_subnet" "internal" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+# -------------------------
+# Network Security Group (SSH-only)
+# -------------------------
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.prefix}nsg-demo"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range           = "*"
+    destination_port_range      = "22"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+  }
+}
+
+# -------------------------
+# Public IP
+# -------------------------
+resource "azurerm_public_ip" "public_ip" {
+  name                = "${var.prefix}-public-ip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
 ## Creating network interfaces with name, location and rg
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
@@ -51,44 +83,54 @@ resource "azurerm_network_interface" "main" {
     name                          = "testconfiguration1"
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
 
-## Creating azure VM with name, location, rg, network interface, vm size
-resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
+#----------------------------
+# NIC and NSG Association
+#----------------------------
+resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.main.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# -------------------------
+# Linux Virtual Machine
+# -------------------------
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "${var.prefix}-demo-vm"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
   network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_B1ls"
 
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  delete_os_disk_on_termination = true
+  # SSH authentication (no passwords)
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("./id_rsa.pub") # must exist on your system
+  }
 
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  delete_data_disks_on_termination = true
+  disable_password_authentication = true
 
-  storage_image_reference {
+  os_disk {
+    name                 = "demo-osdisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
+    sku       = "22_04-lts-gen2"
     version   = "latest"
   }
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
+
+  computer_name = "demo-vm"
+
   tags = {
     environment = "staging"
+    owner       = "devops"
   }
 }
